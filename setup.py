ed263f0888fd04f584a496d51b4095e4c5c1e5dc
Khalid Zuberi <kzuberi@gmail.com>
2014-06-01 10:55:53 -0400

    pep 440 style version #

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ipython-cytoscape.js",
-    version = "0.1.0-dev",
+    version = "0.1.0.dev1",
     description = "Cytoscape.js network visualization widget for the IPython Notebook",
     author = "Khalid Zuberi",
     author_email = "kzuberi@gmail.com",
