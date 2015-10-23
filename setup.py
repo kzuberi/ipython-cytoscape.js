3ba230cda7715baddc0413adaa6a6508a230cb9f
Khalid Zuberi <kzuberi@gmail.com>
2015-10-22 20:18:01 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ipython-cytoscape.js",
-    version = "0.1.0.dev1",
+    version = "0.2a1",
     description = "Cytoscape.js network visualization widget for the IPython Notebook",
     author = "Khalid Zuberi",
     author_email = "kzuberi@gmail.com",
